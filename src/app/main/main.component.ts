f038d06bd17c308578181e322f3dd1f841b33da3
TUAN-PC <anhtuan281290@gmail>
2018-06-20 21:00:59 +0700

    load thông tin user đăng nhập

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,20 +3,23 @@ import { AuthenService } from '../core/service/authen.service';
 import { UrlConstants } from '../core/common/url.constants';
 import { Router } from '@angular/router';
 import { UtilityService } from '../core/service/utility.service';
+import { LoggedInUser } from '../core/domain/loggin.user';
+import { SystemConstants } from '../core/common/system.constants';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+  data : LoggedInUser;
   constructor(private authenService : AuthenService,private router: Router,private utilityService :UtilityService) { }
 
   ngOnInit() {
+    this.data =JSON.parse(localStorage.getItem(SystemConstants.CURRENT_USER));
   }
   logout(){
     this.authenService.logout();
-    //this.router.navigate([UrlConstants.LOGIN]);
     this.utilityService.navigateToLogin();
   }
+
 }
